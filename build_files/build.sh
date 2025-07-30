583084a128fa9961bfa2d06901bf1871ab383258
Hadi Chokr <hadichokr@icloud.com>
2025-07-30 20:08:54 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,10 +11,6 @@ dnf5 install -y \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-log "Installing multimedia codecs..."
-dnf5 install -y --skip-broken libavcodec-freeworld --allowerasing
-dnf5 swap -y ffmpeg-free ffmpeg --allowerasing
-
 ### 🔧 KDE Build Dependencies
 log "Installing KDE build dependencies (this might take a while)..."
 cp -r /root /root-back
